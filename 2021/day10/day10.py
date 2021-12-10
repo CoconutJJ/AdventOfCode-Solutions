a9cdd4fb6c33d40331dc67f400ace6f55a84a6f5
David Yue <davidyue5819@gmail.com>
2021-12-10 07:44:11 -0500

    update template to autofetch input if not exists

--- 2021/day10/day10.py
+++ 2021/day10/day10.py
@@ -1,6 +1,7 @@
 from typing import List
 from sys import argv
-
+import requests
+import os
 
 def part1(lines: List[str]):
 
@@ -90,12 +91,99 @@ def part2(lines: List[str]):
 
     return sorted(scores)
 
+# region Fetch Input and Run
+YEAR = 2021
+
+def sessionKey():
+    cwd = os.getcwd()
+    curr = cwd
+    while not os.path.exists("SESSION"):
+        os.chdir(curr := os.path.join(curr, ".."))
+        if curr == "/":
+            print("Could not find SESSION file!")
+            exit(1)
+    
+    key = open("SESSION", "r")
+    os.chdir(cwd)
+    return key.read().strip("\n")
+
+def fetchPuzzleInput():
+    print("Fetching puzzle input...")
+
+    if os.path.isfile("input.txt"):
+        print("Using cached input...")
+        fp = open("input.txt", "r")
+        lines = fp.readlines()
+        lines = [r.strip("\n") for r in lines]
+        return lines
+
+    s = requests.Session()
+
+    s.cookies.set("session", sessionKey(), domain=".adventofcode.com")
+
+    filename, _ = argv[0].split(".")
+
+    dayNo = None
+
+    if filename.startswith("day"):
+        try:
+            dayNo = int(filename[3:])
+        except:
+            while (dayNo := input(
+                    "Error parsing day number. Please enter the day number: ")):
+                try:
+                    dayNo = int(dayNo)
+                    break
+                except:
+                    print("Invalid Day Number")
+                    continue
+
+    URL = "https://adventofcode.com/%d/day/%d/input" % (YEAR, dayNo)
+    print("Downloading input for day %d" % dayNo)
+    body = s.get(URL, headers={
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0"
+    })
+
+    fp = open("input.txt", "w")
+    fp.write(body.content.decode("utf-8"))
+    fp.close()
+
+    lines = body.content.decode("utf-8").splitlines()
+    lines = [r.strip("\n") for r in lines]
+
+    return lines
+
 
 if __name__ == "__main__":
 
-    fp = open(argv[1], "r")
-    lines = fp.readlines()
-    lines = [r.strip("\n") for r in lines]
+    if len(argv) < 2:
+        lines = fetchPuzzleInput()
+    else:
+        fp = open(argv[1], "r")
+        lines = fp.readlines()
+        lines = [r.strip("\n") for r in lines]
 
-    # print(part1(lines))
-    print(part2(lines))
+    while (part := input("Which part to run ? [1 (default)/2]: ")):
+
+        part = part.strip("\n")
+
+        if len(part) == 0:
+            print(part1(lines))
+            break
+
+        try:
+            part = int(part)
+        except:
+            print("Invalid part number")
+            continue
+
+        if part == 1:
+            print(part1(lines))
+        elif part == 2:
+            print(part2(lines))
+        else:
+            print("Invalid part number")
+            continue
+
+        break
+# endregion
--- 2021/day9/day9.py
+++ 2021/day9/day9.py
@@ -1,5 +1,7 @@
 from typing import List
 from sys import argv
+import requests
+import os
 from random import randint
 
 def inputGenerator():
@@ -95,12 +97,99 @@ def part2(lines: List[str]):
 
     return top3
 
+# region Fetch Input and Run
+YEAR = 2021
+
+def sessionKey():
+    cwd = os.getcwd()
+    curr = cwd
+    while not os.path.exists("SESSION"):
+        os.chdir(curr := os.path.join(curr, ".."))
+        if curr == "/":
+            print("Could not find SESSION file!")
+            exit(1)
+    
+    key = open("SESSION", "r")
+    os.chdir(cwd)
+    return key.read().strip("\n")
+
+def fetchPuzzleInput():
+    print("Fetching puzzle input...")
+
+    if os.path.isfile("input.txt"):
+        print("Using cached input...")
+        fp = open("input.txt", "r")
+        lines = fp.readlines()
+        lines = [r.strip("\n") for r in lines]
+        return lines
+
+    s = requests.Session()
+
+    s.cookies.set("session", sessionKey(), domain=".adventofcode.com")
+
+    filename, _ = argv[0].split(".")
+
+    dayNo = None
+
+    if filename.startswith("day"):
+        try:
+            dayNo = int(filename[3:])
+        except:
+            while (dayNo := input(
+                    "Error parsing day number. Please enter the day number: ")):
+                try:
+                    dayNo = int(dayNo)
+                    break
+                except:
+                    print("Invalid Day Number")
+                    continue
+
+    URL = "https://adventofcode.com/%d/day/%d/input" % (YEAR, dayNo)
+
+    body = s.get(URL, headers={
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0"
+    })
+
+    fp = open("input.txt", "w")
+    fp.write(body.content.decode("utf-8"))
+    fp.close()
+
+    lines = body.content.decode("utf-8").splitlines()
+    lines = [r.strip("\n") for r in lines]
+
+    return lines
+
 
 if __name__ == "__main__":
 
-    fp = open(argv[1], "r")
-    lines = fp.readlines()
-    lines = [r.strip("\n") for r in lines]
-    # print(inputGenerator())
-    # part1(lines)
-    print(part2(lines))
+    if len(argv) < 2:
+        lines = fetchPuzzleInput()
+    else:
+        fp = open(argv[1], "r")
+        lines = fp.readlines()
+        lines = [r.strip("\n") for r in lines]
+
+    while (part := input("Which part to run ? [1 (default)/2]: ")):
+
+        part = part.strip("\n")
+
+        if len(part) == 0:
+            print(part1(lines))
+            break
+
+        try:
+            part = int(part)
+        except:
+            print("Invalid part number")
+            continue
+
+        if part == 1:
+            print(part1(lines))
+        elif part == 2:
+            print(part2(lines))
+        else:
+            print("Invalid part number")
+            continue
+
+        break
+# endregion
